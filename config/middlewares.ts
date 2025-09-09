cb5d45a221c4eee4efffb2dd779f6e46435a4a0e
Biz <yornchanpenh666@gmail.com>
2025-09-09 13:13:10 +0700

    allow all core

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -51,4 +51,12 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', '*'], // allowed domains
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
 ];
